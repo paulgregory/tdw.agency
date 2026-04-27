97beac8e74189e796a7f23c6dcde4e4f26b05581
Paul Gregory <paul@monki.info>
2026-04-27 21:07:55 +0100

    Mobile fixes

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import { Button } from '../Button/Button'
 
 export function Hero({ title, subtitle, desktopNarrow = false }) {
   return (
-    <div className='flex w-full min-h-[400px] sm:min-h-[350px] lg:min-h-[450px] flex-col justify-end items-start gap-6 self-stretch hero-shading pt-0 pb-12'>
+    <div className='flex w-full min-h-[400px] sm:min-h-[350px] md:min-h-[400px] lg:min-h-[450px] flex-col justify-end items-start gap-6 self-stretch hero-shading pt-0 pb-12'>
       <div className='container'>
         <div className='grid grid-cols-1 md:grid-cols-5 gap-8 lg:gap-12'>
           <h1 className={classNames('text-3xl lg:text-4xl xl:text-5xl leading-[1.4] font-semibold text-white row-start-1', desktopNarrow ? 'md:col-span-3' : 'md:col-span-4')}>
