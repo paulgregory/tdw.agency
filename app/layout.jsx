eae4eb05580fa140c2c4830583a1045c3039239d
Paul Gregory <paul@monki.info>
2026-05-05 09:21:57 +0100

    GTM

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '../styles/tailwind.css'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata = {
   title: 'Two Degrees West \\\\ Digital Consultancy',
@@ -9,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='scroll-smooth bg-navy'>
+      <GoogleTagManager gtmId='GTM-5WX37LD6' />
       <body className='min-h-dvh before:fixed before:inset-0 before:bg-navy before:bg-[url(/img/topography.svg)] before:bg-center before:bg-no-repeat before:bg-cover'>
         <div className='animate-fadein'>
           {children}
